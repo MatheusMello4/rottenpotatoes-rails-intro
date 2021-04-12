7c42be562a09ac7de8efebbddf5aeef8d9bd3422
Matheus Mello <matheus.tellesdearaujomello@faulkner.edu>
2021-04-12 15:07:35 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.with_ratings(ratings_list)
+    @movies = Movie.with_ratings(ratings_list, sort_by)
     @all_ratings = Movie.all_ratings
     @ratings_to_show = ratings_hash
+    @sort_by = sort_by
    
     
   end
@@ -46,10 +47,16 @@ class MoviesController < ApplicationController
     Hash[ratings_list.collect {|item| [item,"1"]}]
   end
   
+  
+  
   def ratings_list
     params[:ratings]&.keys || Movie.all_ratings
   end
 
+  def sort_by
+    params[:sort_by] || 'id'
+  end
+  
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
